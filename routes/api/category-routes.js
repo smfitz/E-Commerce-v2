bedc9792c91b996362dfa5100a330cb483b383a6
Sean M Fitzpatrick <s3an.m4l.f1tz@gmail.com>
2023-02-22 14:33:23 -0500

    updated route practice that actually works for categories woohoo

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -4,8 +4,23 @@ const { Category, Product } = require('../../models');
 // The `/api/categories` endpoint
 
 router.get('/', (req, res) => {
-  // find all categories
-  // be sure to include its associated Products
+  Category.findAll({
+    include: {
+      model: Product,
+      attributes: ['id', 'product_name', 'price', 'stock', 'category_id']
+    }
+  })
+    .then(dbCategoryData => {
+      if(!dbCategoryData) {
+        res.status(404).json({message: 'nothing matched you request'})
+        return;
+      }
+      res.json(dbCategoryData);
+    })
+    .catch(e => {
+      console.log(e);
+      res.status(500).json(e)
+    });
 });
 
 router.get('/:id', (req, res) => {
